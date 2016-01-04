6c00ae444272afc9a6616eda5b571c0f1e916f69
drgef <thedrgef@gmail.com>
2016-01-05 02:34:22 +0300

    stop at listing 11.9

--- spec/models/relationship_spec.rb
+++ spec/models/relationship_spec.rb
@@ -16,4 +16,14 @@ describe Relationship do
   	it { expect(relationship.follower).to eq follower }
   	it { expect(relationship.followed).to eq followed }
   end
+
+  describe "when followed id is not present" do
+    before { relationship.followed_id = nil }
+    it { should_not be_valid }
+  end
+
+  describe "when follower id is not present" do
+    before { relationship.follower_id = nil }
+    it { should_not be_valid }
+  end
 end
