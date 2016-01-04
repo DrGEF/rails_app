3c4fee9275cea0dc91b40d37845fec5e63ac8ff9
nik <thedrgef@gmail.com>
2016-01-04 13:29:01 +0300

    stop at 11.1.3

---
+++ spec/models/relationship_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+
+describe Relationship do
+
+  let(:follower) { FactoryGirl.create(:user) }
+  let(:followed) { FactoryGirl.create(:user) }
+  let(:relationship) { follower.relationships.build(followed_id: followed.id) }
+
+  subject { relationship }
+
+  it { should be_valid }
+
+  describe 'follower methods' do
+  	it { should respond_to(:follower) }    
+  	it { should respond_to(:followed) }
+  	it { expect(relationship.follower).to eq follower }
+  	it { expect(relationship.followed).to eq followed }
+  end
+end
